684f4801a3397e919cead1684a0d1db5942aa6c3
sawirose <skassa@unomaha.edu>
2015-04-16 19:00:25 -0500

    -removed links to submission

--- Teacher/app.js
+++ Teacher/app.js
@@ -4,10 +4,10 @@
 angular.module('Teacher',['ui.bootstrap'])
 .controller('MainCtrl', function($scope){
         $scope.assignments = [
-            {title:'Assignment 1' , due : '02/01/2015' , points : '10' , submission:'submission.html'},
-            {title:'Assignment 2' , due : '02/23/2015' , points : '25' , submission:'submission.html'},
-            {title:'Assignment 3' , due : '03/18/2015' , points : '25' , submission:'submission.html'},
-            {title:'Assignment 4' , due : '04/01/2015' , points : '50' , submission:'submission.html'}
+            {title:'Assignment 1' , due : '02/01/2015' , points : '10' , submission:'#'},
+            {title:'Assignment 2' , due : '02/23/2015' , points : '25' , submission:'#'},
+            {title:'Assignment 3' , due : '03/18/2015' , points : '25' , submission:'#'},
+            {title:'Assignment 4' , due : '04/01/2015' , points : '50' , submission:'#'}
         ] ;
 
         $scope.newAssignment = {};
@@ -17,6 +17,5 @@ angular.module('Teacher',['ui.bootstrap'])
             $scope.assignments.push($scope.newAssignment);
             $scope.newAssignment = {};
         }
-
 
     });
